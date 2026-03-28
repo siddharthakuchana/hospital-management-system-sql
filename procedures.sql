3fedc6593e6ee4d0c3364c562e66f9bb66864e47
Siddhartha Kuchana <siddharthakuchana0207@gmail.com>
2026-03-28 12:15:06 +0530

    Add stored procedure to insert new patient

--- procedures.sql
+++ procedures.sql
@@ -1,1 +1,13 @@
+DELIMITER //
 
+CREATE PROCEDURE AddPatient(
+    IN p_name VARCHAR(100),
+    IN p_age INT,
+    IN p_gender VARCHAR(10)
+)
+BEGIN
+    INSERT INTO patients(name, age, gender)
+    VALUES (p_name, p_age, p_gender);
+END //
+
+DELIMITER ;
